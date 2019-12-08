eb3227063679155f96b2d4910d02e9db93babf79
Nico550363 <y5ut010@gmail.com>
2019-12-08 20:39:51 +0900

    自動更新機能修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,7 +9,7 @@ $(function () {
                 ${message.user_name}
               </div>
               <div class="upper-message__date">
-                ${message.date}
+                ${message.created_at}
               </div>
             </div>
             <div class="lower-message">
@@ -73,3 +73,4 @@ $(function () {
   };
   setInterval(reloadMessages, 5000);    
 });
+
